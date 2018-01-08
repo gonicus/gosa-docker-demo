264890f77d067dda9920e5718b01d5c7769afcc6
Cajus Pollmeier <pollmeier@gonicus.de>
2018-01-08 15:36:30 +0100

    Updated starter

--- openldap/start.sh
+++ openldap/start.sh
@@ -1,20 +1,16 @@
-#!/bin/sh
+#!/bin/bash -x
 
 # Check for base ldif container
 echo -n "[SLAPD] checking for base configuration: "
-if [ ! -f /var/lib/ldap/alock ]; then
-    chown -R root:$LDAP_GROUP /etc/ldap
-
+if [ ! -f /var/lib/ldap/data.mdb ]; then
     if [ -f /provision/base.ldif ]; then
         cp /usr/share/doc/slapd/examples/DB_CONFIG /var/lib/ldap/DB_CONFIG
 
-        slapadd -l /provision/base.ldif -b dc=example,dc=net &> /tmp/slapadd.log
-
+        slapadd -q -l /provision/base.ldif -b dc=example,dc=net &> /tmp/slapadd.log
         if [ $? -ne 0 ]; then
             echo "failed\n\n"
             cat /tmp/slapadd.log
         else
-            chown -R $LDAP_USER:$LDAP_GROUP /var/lib/ldap
             echo "provisioned"
         fi
     else
@@ -24,4 +20,5 @@ else
     echo "found"
 fi
 
+chown -R $LDAP_USER:$LDAP_GROUP /var/lib/ldap
 exec /usr/sbin/slapd -u openldap
